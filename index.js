bace831e6be43799e02e4f4adbe9e358e96ae166
bungularbike <rongjackmiles@gmail.com>
2020-09-10 16:09:40 +0800

    Auth test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var firebaseConfig = {
     apiKey: "AIzaSyA6uzX2X3dFEOz86Cwp3ZxeAreKR2XY7fo",
-    authDomain: "hkis-interim.firebaseapp.com",
+    authDomain: "hkisrobotics.com",
     databaseURL: "https://hkis-interim.firebaseio.com",
     projectId: "hkis-interim",
     storageBucket: "hkis-interim.appspot.com",
@@ -10,3 +10,14 @@ var firebaseConfig = {
  };
  firebase.initializeApp(firebaseConfig);
  firebase.analytics();
+
+var provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParamters({"hd": "hkis.edu.hk"});
+firebase.auth().signInWithPopup(provider).then(function(result) {
+	var token = result.credential.accessToken;
+	console.log(token);
+	var user = result.user;
+}).catch(function(error) {
+	var errorMessage = error.message;
+	console.log(errorMessage);
+});

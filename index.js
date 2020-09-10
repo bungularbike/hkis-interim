cadf35a1c1b7219d2a6d594857509c1f38f8900b
bungularbike <rongjackmiles@gmail.com>
2020-09-10 13:01:11 +0800

    Init

---
+++ index.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA6uzX2X3dFEOz86Cwp3ZxeAreKR2XY7fo",
+    authDomain: "hkis-interim.firebaseapp.com",
+    databaseURL: "https://hkis-interim.firebaseio.com",
+    projectId: "hkis-interim",
+    storageBucket: "hkis-interim.appspot.com",
+    messagingSenderId: "623891731224",
+    appId: "1:623891731224:web:cfc8c3ee29e20908c8c46d",
+    measurementId: "G-E8XR3ZS8QZ"
+ };
+ firebase.initializeApp(firebaseConfig);
+ firebase.analytics();

2f85b62ba65682437feb3be8843df5a27eda84ea
bungularbike <rongjackmiles@gmail.com>
2020-09-12 14:38:09 +0800

    Dash 7

--- dash.js
+++ dash.js
@@ -17,10 +17,15 @@ var storage = firebase.storage();
 
 var formatter = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" });
 
+var starFilled = '<svg width = "1.25em" height = "1.25em" viewBox = "0 0 16 16" fill = "currentColor" xmlns = "http://www.w3.org/2000/svg"><path d = "M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg>';
+var starEmpty = '<svg width = "1.25em" height = "1.25em" viewBox = "0 0 16 16" fill = "currentColor" xmlns = "http://www.w3.org/2000/svg" style = "vertical-align: middle"><path fill-rule = "evenodd" d = "M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" /></svg>';
+var currentStarred = [];
+
 db.collection("interims").orderBy("id").get().then(function(qS) {
     $("h3.navbar-brand").html("Explore Trips (" + qS.size + ")");
     qS.forEach(function(doc) {
         var trip = doc.data();
+        var id = trip.id;
         var categories = "";
         if (trip.service) {
             categories = "Service";
@@ -39,18 +44,56 @@ db.collection("interims").orderBy("id").get().then(function(qS) {
         }
         var price = formatter.format(trip.price);
         price = price.substring(0, price.length - 3);
-        $(".card-columns").append("<div class = 'card' id = 'trip" + doc.id + "' tabindex = '0' ontouchstart = ''><div class = 'card-body'><h4 class = 'card-title'>" + (trip.new ? "<span class = 'h4' style = 'color: red'>NEW </span>" : "") + trip.name + "</h6><h6 class = 'card-text'>" + categories + "</h6><h6 class = 'card-text'>" + price + "</h6><p class = 'card-text mb-0'>" + trip.description + "</p></div></div>");
-        $("#trip" + doc.id).keydown(function(event) {
+        $(".card-columns").append("<div class = 'card tripCard' tabindex = '0' ontouchstart = '' id = 'tripCard" + id + "'><div class = 'card-header'><div class = 'card-text toggleStar d-inline-block' id = 'toggleStar" + id + "' tabindex = '0' style = 'margin-bottom: 0 !important'>" + starEmpty + "</div></div><div class = 'card-body trip'><h4 class = 'card-title'>" + (trip.new ? "<span class = 'h4' style = 'color: red'>NEW </span>" : "") + trip.name + "</h6><h6 class = 'card-text'>" + categories + "</h6><h6 class = 'card-text'>HKD" + price + "</h6><p class = 'card-text mb-0'>" + trip.description + "</p></div></div>");
+        $clamp($("#tripCard" + id + " p")[0], { clamp: 5, useNativeClamp: false });
+        $("#tripCard" + id).keypress(function(event) {
             if (event.keyCode == 32) {
-                $("#trip" + doc.id).addClass("active");
+                $("#tripCard" + id).trigger("click");
                 return false;
             }
         });
-        $("#trip" + doc.id).keyup(function(event) {
-            $("#trip" + doc.id).removeClass("active");
+        currentStarred[id - 1] = false;
+        $("#toggleStar" + id).keypress(function(event) {
+            if (event.keyCode == 32) {
+                $("#toggleStar" + id).trigger("click");
+                return false;
+            }
         });
-        storage.ref("previews/" + doc.id + ".jpg").getDownloadURL().then(function(url) {
-            $("#trip" + doc.id).prepend("<img src = \"" + url + "\" class = 'card-img-top'>");
+        $("#tripCard" + id).click(function() {
+           // alert("");
+        });
+        $("#toggleStar" + id).click(function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            if (!currentStarred[id - 1]) {
+                $("#toggleStar" + id).html(starFilled);
+                currentStarred[id - 1] = true;
+           } else {
+                $("#toggleStar" + id).html(starEmpty);
+                currentStarred[id - 1] = false;
+           }
+        });
+        storage.ref("previews/" + id + ".jpg").getDownloadURL().then(function(url) {
+            $("#tripCard" + id).prepend("<div class = 'starHolder toggleStar' id = 'toggleStar" + id + "' tabindex = '0'>" + starEmpty + "</div>");
+            $("#tripCard" + id).prepend("<img src = \"" + url + "\" style = 'position: relative' class = 'card-img-top trip'>");
+            $("#tripCard" + id + " .card-header").remove();
+            $("#toggleStar" + id).keypress(function(event) {
+                if (event.keyCode == 32) {
+                    $("#toggleStar" + id).trigger("click");
+                    return false;
+                }
+            });
+            $("#toggleStar" + id).click(function(event) {
+                event.preventDefault();
+                event.stopPropagation();
+                if (!currentStarred[id - 1]) {
+                    $("#toggleStar" + id).html(starFilled);
+                    currentStarred[id - 1] = true;
+               } else {
+                    $("#toggleStar" + id).html(starEmpty);
+                    currentStarred[id - 1] = false;
+               }
+            });
         }).catch(function(error) {
             if (error.code != "storage/object-not-found") {
                 alert(error.message);
@@ -61,6 +104,20 @@ db.collection("interims").orderBy("id").get().then(function(qS) {
     alert(error.message);
 });
 
+var toggleStar = false;
+$("#filterStar").click(function() {
+    if (!toggleStar) {
+        $("#filterStar").html(starFilled);
+        toggleStar = true;
+    } else {
+        $("#filterStar").html(starEmpty);
+        toggleStar = false;
+    }
+});
+
+$("#fade").remove();
+$("#userName").html("Jack Rong");
+/*
 firebase.auth().onAuthStateChanged(function(user) {
 	if (!user) {
 		window.open("index.html", "_self");
@@ -86,4 +143,4 @@ $("#signOut").click(function() {
     $("#signOut").attr("disabled", true);
     firebase.auth().signOut();
 });
-
+*/
